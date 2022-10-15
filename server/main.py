c3fbd711988df9910f97651aa39b3e4b764621bc
Kid Neild <kidneild@Kids-MacBook-Pro.local>
2022-10-15 13:19:50 -0400

    added main.py for server

---
+++ server/main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    '*',
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+async def main():
+    return {"message": "Hello World"}
+
+@app.get("/picture")
+async def main():
+    return {"date":"1999", "img_src":"https://www.princeton.edu/sites/default/files/styles/half_1x/public/images/2022/02/KOA_Nassau_2697x1517.jpg?itok=fAtaOLnw"}
+
+
