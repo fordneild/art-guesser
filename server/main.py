d6e636b8659f49d605ab6ea70fde40f3fcf76838
fordneild <hanfordn35@gmail.com>
2022-10-15 14:23:18 -0400

    formatted main

--- server/main.py
+++ server/main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 origins = [
     "http://localhost",
     "http://localhost:8080",
-    '*',
+    "*",
 ]
 
 app.add_middleware(
@@ -23,17 +23,19 @@ class Item(BaseModel):
     date: str
 
 
-
 @app.get("/")
 async def main():
     return {"message": "Hello World"}
 
+
 @app.get("/picture")
 async def main():
-    return {"date":"1999", "img_src":"https://www.princeton.edu/sites/default/files/styles/half_1x/public/images/2022/02/KOA_Nassau_2697x1517.jpg?itok=fAtaOLnw"}
+    return {
+        "date": "1999",
+        "img_src": "https://www.princeton.edu/sites/default/files/styles/half_1x/public/images/2022/02/KOA_Nassau_2697x1517.jpg?itok=fAtaOLnw",
+    }
+
 
 @app.post("/date")
 async def create_item(item: Item):
     return item
-
-
