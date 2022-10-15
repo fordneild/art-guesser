4342d895c92e46585be93349e1d53b9365d90f4f
fordneild <hanfordn35@gmail.com>
2022-10-15 19:00:32 -0400

    added service

--- server/service.py
+++ server/service.py
@@ -1,3 +1,18 @@
-class ObjectService:
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+from models import Object
+
+
+class BaseService:
+    def __init__(self, db: Session) -> None:
+        self.db = db
+
+
+class ObjectService(BaseService):
     def list(self):
-        print("hello world")
+        a = self.db.query(Object).limit(5).all()
+        import pdb
+
+        pdb.set_trace()
+        return a
