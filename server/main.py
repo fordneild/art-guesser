9d7b9f2a2eada518a7835b6c4f60c074a48a687f
Kid Neild <kidneild@Kids-MacBook-Pro.local>
2022-10-15 19:02:24 -0400

    added posts

--- server/main.py
+++ server/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
+
 
 app = FastAPI()
 
@@ -19,8 +22,16 @@ app.add_middleware(
 )
 
 
-class Item(BaseModel):
-    date: str
+class date(BaseModel):
+    date: int
+
+
+class artist(BaseModel):
+    artist: str
+
+
+class style(BaseModel):
+    style: str
 
 
 @app.get("/")
@@ -37,5 +48,23 @@ async def main():
 
 
 @app.post("/date")
-async def create_item(item: Item):
-    return item
+async def date(item: date):
+    real_date = 1999
+    guessed_date = item.date
+    remainder = real_date - guessed_date
+    content = jsonable_encoder({"remainder": remainder})
+    return JSONResponse(content=content)
+
+
+@app.post("/artist")
+async def artist(item: artist):
+    real_artist = "Ford"
+    content = jsonable_encoder({"artist": real_artist})
+    return JSONResponse(content=content)
+
+
+@app.post("/style")
+async def style(item: style):
+    real_style = "cool"
+    content = jsonable_encoder({"style": real_style})
+    return JSONResponse(content=content)
