f5811caaa5a83cb32de191254b0ed130f7a5cbc8
fordneild <hanfordn35@gmail.com>
2022-10-15 14:19:51 -0400

    fromatted main

--- server/main.py
+++ server/main.py
@@ -1,12 +1,15 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from server.domain import Object
+
+from server.service import ObjectService
 
 app = FastAPI()
 
 origins = [
     "http://localhost",
     "http://localhost:8080",
-    '*',
+    "*",
 ]
 
 app.add_middleware(
@@ -22,8 +25,15 @@ app.add_middleware(
 async def main():
     return {"message": "Hello World"}
 
+
 @app.get("/picture")
 async def main():
-    return {"date":"1999", "img_src":"https://www.princeton.edu/sites/default/files/styles/half_1x/public/images/2022/02/KOA_Nassau_2697x1517.jpg?itok=fAtaOLnw"}
+    return {
+        "date": "1999",
+        "img_src": "https://www.princeton.edu/sites/default/files/styles/half_1x/public/images/2022/02/KOA_Nassau_2697x1517.jpg?itok=fAtaOLnw",
+    }
 
 
+@app.get("/objects", response_model=Object)
+async def main():
+    return ObjectService().list()
