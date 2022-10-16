1e478c1e104ba6ddd3cb1f12d94f2c68c6e976d6
fordneild <hanfordn35@gmail.com>
2022-10-16 17:04:22 -0400

    removed comment

--- server/views/object.py
+++ server/views/object.py
@@ -5,7 +5,7 @@ from fastapi_utils.cbv import cbv
 from fastapi_utils.inferring_router import InferringRouter
 from views.base import BaseView
 
-router = InferringRouter()  # Step 1: Create a router
+router = InferringRouter()
 
 
 @cbv(router)
