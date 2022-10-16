481749cf985a5d4799556c742da670b530e56af8
fordneild <hanfordn35@gmail.com>
2022-10-16 17:03:33 -0400

    added domain

--- server/domain.py
+++ server/domain.py
@@ -1,8 +1,15 @@
+from typing import Dict, List
 from pydantic import BaseModel
 
 
-class ObjectDomain(BaseModel):
-    title: str
+class ObjectImage(BaseModel):
+    src: str
+    width: int
+    height: int
 
-    class Config:
-        orm_mode = True
+
+class ObjectDomain(BaseModel):
+    id: int
+    title: str
+    terms: List[Dict[str, str]]
+    image: ObjectImage
