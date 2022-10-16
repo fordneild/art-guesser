c696818470ef5bbc7dedd7ffe05ca736a37d3970
fordneild <hanfordn35@gmail.com>
2022-10-16 17:03:48 -0400

    updated list method in object service

--- server/service.py
+++ server/service.py
@@ -1,7 +1,10 @@
-from sqlalchemy import select
+from typing import Optional
 from sqlalchemy.orm import Session
 
-from models import Object
+from models import Object, ObjectsTerm, PublishedImage
+from sqlalchemy import func
+
+from domain import ObjectDomain, ObjectImage
 
 
 class BaseService:
@@ -10,6 +13,48 @@ class BaseService:
 
 
 class ObjectService(BaseService):
-    def list(self):
-        a = self.db.query(Object).limit(5).all()
-        return a
+    classifications = ("Painting", "Drawing")
+
+    def list(self, limit: Optional[int] = 5):
+        terms_agg = func.array_agg(
+            func.json_build_object(ObjectsTerm.termtype, ObjectsTerm.term),
+        ).label("terms")
+        objects_with_terms = (
+            self.db.query(
+                Object.objectid,
+                Object.title,
+                terms_agg,
+            )
+            .filter(Object.classification.in_(self.classifications))
+            .join(
+                ObjectsTerm,
+                ObjectsTerm.objectid == Object.objectid,
+            )
+            .group_by(Object.objectid)
+            .subquery()
+        )
+        objects = (
+            self.db.query(
+                objects_with_terms,
+                PublishedImage.iiifthumburl,
+                PublishedImage.width,
+                PublishedImage.height,
+            )
+            .join(
+                PublishedImage,
+                PublishedImage.depictstmsobjectid == objects_with_terms.c.objectid,
+            )
+            .order_by(func.random())
+            .limit(limit)
+            .all()
+        )
+
+        return [
+            ObjectDomain(
+                id=objectid,
+                title=title,
+                terms=terms,
+                image=ObjectImage(src=img_src, width=img_width, height=img_height),
+            )
+            for objectid, title, terms, img_src, img_width, img_height in objects
+        ]
